c118c4fe86e22f55f6f0091c7c9998c8f3ac7e35
Maleeha02 <maleeha.salyani@gmail.com>
2023-05-04 08:47:08 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,50 +2,48 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer 10 bytes at a time, starting with
- * the byte possition, then showing the hex content,
- * then displaying printable characters.
+ * print_buffer - Prints a buffer
  * @b: buffer
  * @size: size
  */
 
 void print_buffer(char *b, int size)
 {
-	int byte, index;
-
-	for (byte = 0; byte < size; byte += 10)
+	if (size <= 0)
 	{
-		printf("%08x: ", byte);
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				printf(" ");
-
-			else
-				printf("%02x", *(b + index + byte));
-
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
-		}
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
-
-			else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
-
-			else
-				printf(".");
-		}
-
-		if (byte >= size)
-			continue;
-		printf('\n');
+		printf("\n");
+		return;
 	}
 
-	if (size <= 0)
-		printf('\n');
+	int i, j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x ", i);
+		for (j = i; j < i + 10 && j < size; j += 2)
+		{
+			printf("%02x", (unsigned char)b[j]);
+			if (j + 1 < size)
+			{
+				printf("%02x ", (unsigned char)b[j + 1]);
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf(" ");
+		for (j = i; j < i + 10 && j < size; j++)
+		{
+			if (isprint(b[j]))
+			{
+				printf("%c", b[j]);
+			}
+			else
+			{
+				printf(".");
+			}
+		}
+		printf("\n");
+	}
 }
