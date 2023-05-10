24b4b9ef9300f94305c0e10b9352d00c9a86c75b
Maleeha02 <maleeha.salyani@gmail.com>
2023-05-10 00:21:07 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - Function that checks if a string is palindrome
+ * @s: String to be reversed
+ * Return: 1 if string is palindrome, 0 if otherwise
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: String length to be calculated
+ * Return: Length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - Checks the characters recursively for palindrome
+ * @s: String to be checked
+ * @i: Iterator
+ * @len: Length of the string
+ * Return: 1 if palindrome, 0 if otherwise
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
