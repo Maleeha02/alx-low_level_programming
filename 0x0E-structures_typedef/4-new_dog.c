46b0b0b002ef191da2c282904bc44f49e2f010ea
Maleeha02 <maleeha.salyani@gmail.com>
2023-05-22 07:11:45 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,7 +20,7 @@ int _strlen(char *str)
 }
 
 /**
- * *strcpy - Copies a string pointed to by src, including the
+ * *_strcpy - Copies a string pointed to by src, including the
  * terminating null byte, to a buffer pointed to by dest.
  * @dest: The buffer storing the string copy
  * @src: The source string
